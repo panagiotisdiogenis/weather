e4c04c62472dffe46ec7ef01b80ee0b108139597
Diogenis Panagiotis <diogenis.panagiotis@startree.ai>
2024-08-07 00:04:06 -0400

    add import

--- src/contexts/WeatherContext.jsx
+++ src/contexts/WeatherContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useEffect, useCallback } from 'react';
+import React, { createContext, useState, useEffect, useCallback, useMemo } from 'react';
 import axios from 'axios';
 
 export const WeatherContext = createContext();
